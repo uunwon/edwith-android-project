ed5df8dd463fbb4cedf8aea31b34f56748e29db0
yunwoon <chea2089@naver.com>
2021-01-10 18:47:57 +0900

    (project D) add viewpager with fragment

--- project-d/app/src/main/java/com/yunwoon/projectd/MainActivity.java
+++ project-d/app/src/main/java/com/yunwoon/projectd/MainActivity.java
@@ -1,17 +1,9 @@
 package com.yunwoon.projectd;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Menu;
-import android.widget.AdapterView;
-import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
--- project-d/app/src/main/java/com/yunwoon/projectd/navigation/ListFragment.java
+++ project-d/app/src/main/java/com/yunwoon/projectd/navigation/ListFragment.java
@@ -7,15 +7,59 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.yunwoon.projectd.R;
+import com.yunwoon.projectd.viewpager.FirstmvFragment;
+import com.yunwoon.projectd.viewpager.SecondmvFragment;
+import com.yunwoon.projectd.viewpager.ThirdmvFragment;
+
+import java.util.ArrayList;
 
 public class ListFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_list, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_list, container, false);
 
-        return root;
+        ViewPager pager = rootView.findViewById(R.id.viewPager);
+        pager.setOffscreenPageLimit(3); // 담을 프래그먼트 갯수 설정
+
+        MoviePagerAdapter adapter = new MoviePagerAdapter(getFragmentManager());
+
+        FirstmvFragment firstmvFragment = new FirstmvFragment();
+        adapter.addItem(firstmvFragment);
+        SecondmvFragment secondmvFragment = new SecondmvFragment();
+        adapter.addItem(secondmvFragment);
+        ThirdmvFragment thirdmvFragment = new ThirdmvFragment();
+        adapter.addItem(thirdmvFragment);
+
+        pager.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    class MoviePagerAdapter extends FragmentStatePagerAdapter {
+        ArrayList<Fragment> items = new ArrayList<>();
+
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item) {
+            items.add(item);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
     }
 }
